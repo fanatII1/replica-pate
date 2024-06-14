31bcf592828ea2c17942a003c363827163b1056b
georgemarwanqana@gmail.com <georgemarwanqana@gmail.com>
2024-06-14 12:55:02 +0200

    router components explanation

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,12 @@ import App from './App.jsx'
 import './index.css'
 import { createBrowserRouter, RouterProvider,} from "react-router-dom";
 
+// the createBrowserRouter function accepts an array of objects
+// each object represents a page(route)
+// the path property represents the route and its name
+// (e.g "/" is for the homepage and /"/login" represents the /login page)
+// the element property represents the top-level component that is for the specific route
+// e.g <App> component is for the homepage
 const router = createBrowserRouter([
   {
     path: "/",
